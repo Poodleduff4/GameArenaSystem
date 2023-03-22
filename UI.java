f2b0e6cbf10d6164b71fdce371d27a10c19bb01a
Poodleduff4 <lukeguardino@yahoo.ca>
2023-03-22 17:00:18 -0400

    Removed weird text

--- UI.java
+++ UI.java
@@ -177,20 +177,6 @@ public class UI {
         f.revalidate();
         f.repaint();
         Component[] comps = f.getContentPane().getComponents();
-<<<<<<< HEAD
-=======
-        for(int i = 0;i < comps.length;i++)
-        {
-            System.out.println(comps[i]);
-            Component[] childs = ((Cart)comps[i]).getComponents();
-            for (int j = 0; j < childs.length; j++) {
-                System.out.println(childs[j]);
-            }
-        }
-
-        //Add button here
-
->>>>>>> origin/Event-Selection
     }
 
     public static void updateSeatInformationPanel(EventSeat seat){
